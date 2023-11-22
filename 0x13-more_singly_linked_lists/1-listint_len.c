1ef4c0bc167dfa8338466a8b373294e0bb801d15
root <gloria.nthenyah@gmail.com>
2023-11-22 06:42:23 -0800

    aa

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,46 +1,20 @@
-#include "main.h"
+#include "lists.h"
+
 /**
-* string_nconcat - a function that concatenates two strings.
-*
-* @s1: first char
-* @s2: secound char
-* @n: unsigned int
-*
-* Return: If the function fails, it should return NULL
-*/
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+ * listint_len - returns the number of elements in a linked lists
+ * @h: linked list of type listint_t to traverse
+ *
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
 {
-	unsigned int x, y, z;
-	char *s;
+	size_t num = 0;
 
+	while (h)
+	{
+	num++;
+	h = h->next;
+	}
 
-	if (s1 == NULL)
-	{
-	x = 0;
-	}
-	else
-	{
-	for (x = 0; s1[x]; ++x)
-	;
-	}
-	if (s2 == NULL)
-	{
-	y = 0;
-	}
-	else
-	{
-	for (y = 0; s2[y]; ++y)
-	;
-	}
-	if (y > n)
-	y = n;
-	s = malloc(sizeof(char) * (x + y + 1));
-	if (s == NULL)
-	return (NULL);
-	for (z = 0; z < x; z++)
-	s[z] = s1[z];
-	for (z = 0; z < y; z++)
-	s[z + x] = s2[z];
-	s[x + y] = '\0';
-	return (s);
+	return (num);
 }
